5e379b855edc1d07f86f03fb0ae718167c6b6dc3
Davide <159887620+Tdavide04@users.noreply.github.com>
2024-04-27 18:54:19 +0200

    ancora in produzione

--- Esercizi/Esercizi4/esercizio4.py
+++ Esercizi/Esercizi4/esercizio4.py
@@ -82,10 +82,10 @@ city_country("Valencia", "Spain")
 #If the calling line includes a value for the number of songs, add that value to the album’s dictionary. 
 #Make at least one new function call that includes the number of songs on an album.
 
-def make_album(artist, title):
+def make_album(artist_name, album_title):
     album_dict = {
-        "artist": artist.title(),
-        "title": title.title()
+        "artist": artist_name.title(),
+        "title": album_title.title()
     }
     print(f"here there are your album {album_dict}")
     return album_dict
@@ -100,22 +100,107 @@ album = make_album("Taylor Swift", "Lover")
 
 #8-8. User Albums: Start with your program from Exercise 8-7. Write a while loop that allows users to enter an album’s artist and title. 
 #Once you have that information, call make_album() with the user’s input and print the dictionary that’s created. Be sure to include a quit value in the while loop.
+'''
+while True:
+    if True:
+        artist_name = input("Enter the artist's name: ")
+        album_title = input("Enter the album title: ")
+        make_album(artist_name, album_title)
+    else:
+        break
+'''
+#8-9. Messages: Make a list containing a series of short text messages. Pass the list to a function called show_messages(), which prints each text message.def 
 
-#8-9. Messages: Make a list containing a series of short text messages. Pass the list to a function called show_messages(), which prints each text message.
+list1 = [
+    "Hello!",
+    "How are you?",
+    "What's up?",
+    "Good morning!"
+    ]
+
+def show_messages(list1: list):
+    print("showing messages: ")
+    for e in list1:
+        print(e)
+
+show_messages(list1)
+
+# -> showing messages:
+# Hello!
+# How are you?
+# What's up?
+# Good morning!
 
 #8-10. Sending Messages: Start with a copy of your program from Exercise 8-9. 
 #Write a function called send_messages() that prints each text message and moves each message to a new list called sent_messages as it’s printed. 
 #After calling the function, print both of your lists to make sure the messages were moved correctly.
+        
+def send_messages(list1):
+    print("sending messages: ")
+    sent_messages = []
+    while list1:
+        current_message = list1.pop()
+        print(current_message)
+        sent_messages.append(current_message)
+    return sent_messages   
+send_messages(list1)
+print(list1)
+
+# -> sending messages:
+# Hello!
+# How are you?
+# What's up?
+# Good morning!
+# []
 
 #8-11. Archived Messages: Start with your work from Exercise 8-10. Call the function send_messages() with a copy of the list of messages. 
 #After calling the function, print both of your lists to show that the original list has retained its messages.
+        
+def send_messages(list1):
+    print("sending messages: ")
+    sent_messages = []
+    while list1:
+        current_message = list1.pop()
+        print(current_message)
+        sent_messages.append(current_message)
+    return sent_messages   
+send_messages(list1)
+print(list1)
+
+# -> non ho capito la consegna
 
 #8-12. Sandwiches: Write a function that accepts a list of items a person wants on a sandwich. 
 #The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that’s being ordered. 
 #Call the function three times, using a different number of arguments each time.
 
+def make_sandwich(*ingredients):
+    print(f"Making a sandwich with the following ingredients: {ingredients}")
+    
+make_sandwich('lettuce', 'tomato', 'cheese')
+make_sandwich('turkey', 'bacon', 'avocado', 'mayonnaise')
+make_sandwich('ham', 'swiss cheese', 'mustard', 'pickles', 'onions')
+
+# -> Making a sandwich with the following ingredients: ('lettuce', 'tomato', 'cheese')
+# Making a sandwich with the following ingredients: ('turkey', 'bacon', 'avocado', 'mayonnaise')
+# Making a sandwich with the following ingredients: ('ham', 'swiss cheese', 'mustard', 'pickles', 'onions')
+
 #8-13. User Profile:  Build a profile of yourself by calling build_profile(), using your first and last names and three other key-value pairs that describe you. 
 #All the values must be passed to the function as parameters. The function then must return a string such as "Eric Crow, age 45, hair brown, weight 67"
+
+def build_profile(fisrt_name, last_name, age, hair_color, weight):
+    profile = {"first name" : fisrt_name,
+               "last name" : last_name,
+               "age" : age,
+               "hair color" : hair_color,
+               "weight" : weight
+               }
+    
+    print(f"your profile is: {profile}")
+    return profile
+
+build_profile("davide", "trischitta", 20, "brown", 79)
+
+# -> your profile is: {'first name': 'davide', 'last name': 'trischitta', 'age': 20, 'hair color': 'brown', 'weight': 79}
 
 #8-14. Cars: Write a function that stores information about a car in a dictionary. The function should always receive a manufacturer and a model name. 
 #It should then accept an arbitrary number of keyword arguments. 
@@ -123,8 +208,29 @@ album = make_album("Taylor Swift", "Lover")
 #Your function should work for a call like this one: car = make_car('subaru', 'outback', color='blue', tow_package=True) 
 #Print the dictionary that’s returned to make sure all the information was stored correctly. 
 
+def make_car(manufacturer, model, color, seats):
+    profile = {"manufacturer" : manufacturer,
+               "model" : model,
+               "color" : color,
+               "seats" : seats
+               }
+    
+    print(f"your car's profile is: {profile}")
+    return profile
+
+car = make_car('subaru', 'outback', color='blue', seats = 4)
+
+# -> your car's profile is: {'manufacturer': 'subaru', 'model': 'outback', 'color': 'blue', 'seats': 4}
+
 #8-15. Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py. 
 #Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.
+
+
+
+
+
+
+
 
 #8-16. Imports: Using a program you wrote that has one function in it, store that function in a separate file. 
 #Import the function into your main program file, and call the function using each of these approaches:
