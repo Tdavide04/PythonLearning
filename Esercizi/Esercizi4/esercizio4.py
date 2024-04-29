2df6a0e8e496ab4564721906d397fe6b92ae5e9d
Davide <159887620+Tdavide04@users.noreply.github.com>
2024-04-29 17:19:49 +0200

    Update esercizio4.py
    non tutti gli esercizi sono svolti poiche alcuni argomenti non li ho capiti e preferirei rivederli in classe

--- Esercizi/Esercizi4/esercizio4.py
+++ Esercizi/Esercizi4/esercizio4.py
@@ -224,21 +224,20 @@ car = make_car('subaru', 'outback', color='blue', seats = 4)
 
 #8-15. Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py. 
 #Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.
+'''
+import printing_functions
+printing_functions.printing("audi", "sedans", color = "red", seats = 2)
 
-
-
-
-
-
-
+#non ho capito perche non funziona
+'''
 
 #8-16. Imports: Using a program you wrote that has one function in it, store that function in a separate file. 
 #Import the function into your main program file, and call the function using each of these approaches:
-'''
-import module_name
-from module_name import function_name
-from module_name import function_name as fn
-import module_name as mn
-from module_name import *
-'''
+
+import printing_functions
+from printing_functions import printing
+from printing_functions import printing as fn
+import printing_functions as mn
+from printing_functions import *
+
 #8-17. Styling Functions: Choose any three programs you wrote for this chapter, and make sure they follow the styling guidelines described in this section.
