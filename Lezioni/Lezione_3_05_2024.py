d6dcfbd4034633cc8c4df9b716ad6c3c3bb63017
Nio4124 <antoniozottola41204@gmail.com>
2024-05-03 19:15:38 +0200

    Update

---
+++ Lezioni/Lezione_3_05_2024.py
@@ -0,0 +1,104 @@
+#Correzione esercizi
+
+def construct_rectangle(area: float) -> list[float]:
+    combos = []
+    half_area = int(area ** 0.5)
+    for width in range(1, area + 1):
+        for height in range(1, area +1):
+            if width * height == area and width >= height:
+                return [width, height]
+
+
+from collections import Counter
+def find_lhs(notes: list[int]) -> int:
+    num_freg = dict(Counter(notes))
+
+    max_length = 0
+    for num in num_freg:
+        if num + 1 in num_freg:
+            somma = num_freg[num]\
+            + num_freg[num +1]
+        if somma >= max_length:
+            max_length = somma
+        
+    return max_length
+
+
+def third_max(gems: list[int]) -> int:
+    gems = sorted(list(set(gems)))
+    if len(gems) >= 3:
+        return gems[2]
+    else:
+        return gems[0]
+    
+def is_subsequence(s: str, t: str) -> bool:
+    s_pointer: int = 0
+    t_pointer: int = 0
+    
+    while s_pointer < len(s)\
+    and t_pointer < len(t):
+        if s[s_pointer] == t[t_pointer]:
+            s_pointer += 1
+        t_pointer += 1
+
+    return s_pointer == len(s)
+
+#Albero binario
+
+def search(array: list[int], x: int) -> int:
+    for i in range(len(array)):
+        if array[i] == x:
+            return i
+    return None
+
+def binary_search(array: list[int], x: int) -> int:
+    low = 0
+    high = len(array)
+    while low < high:
+        mid = (low + high) // 2
+        if array[mid] == x:
+            return mid
+        else:
+            if x > array[mid]:
+                low == mid + 1
+            else:
+                high == mid - 1
+    return None
+
+def visit_tree(tree: dict[int, list[int]], node: int):
+    print(node)
+    left_child, right_child\
+    = tree.get(node, [None, None])
+    if left_child: #is not None
+        visit_tree(tree, left_child)
+    if right_child:
+        visit_tree(tree, right_child)
+    
+tree = {4: [3,5], 3:[2, None], 5:[4.5,6], 2:[None, None], 4.5:[None, None], 6:[None, None]}
+visit_tree(tree, 4)
+
+def visit_tree_interative(tree: dict[int, list[int]], root: int):
+    stack: list[int] = [root] # Last-In-First-Out (LIFO)
+    while stack: #while len(stack) > 0
+        curr_node = stack.pop()
+        print(curr_node)
+        left_child, right_child = tree.get(curr_node, [None, None])
+        if right_child:
+            stack.append(right_child)
+        if left_child:
+            stack.append(left_child)
+
+tree = {4: [3,5], 3:[2, None], 5:[4.5,6], 2:[None, None], 4.5:[None, None], 6:[None, None]}
+visit_tree_interative(tree, 4)
+
+
+
+
+
+
+
+
+
+
+
+
