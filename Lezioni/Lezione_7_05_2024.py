ad9da90dc22b3e01ec78651e191eadae9a23d585
Nio4124 <antoniozottola41204@gmail.com>
2024-05-07 12:39:11 +0200

    Update

--- Lezioni/Lezione_7_05_2024.py
+++ Lezioni/Lezione_7_05_2024.py
@@ -13,6 +13,7 @@ class Person:
 """da ricontrollare a casa la funzione __str__(self)"""
 
 
+
 alice = Person("Alice W.", 45, 165, 43)
 bob = Person("Bob M.", 36, 180, 75)
 philip = Person("Philip", 19, 180, 76)
@@ -35,3 +36,27 @@ for i in range(len(people)):
         index_min_age = i
 
 print(f"il nome della persona più giovane è {people[index_min_age].name} con età {people[index_min_age].age}")
+
+
+
+
+
+
+
+
+
+class Student:
+    def __init__(self, name: str, studyProgram: str, age: float, gender: str):
+        self.name = name
+        self.studyProgram = studyProgram
+        self.age = age
+        self.gender = gender
+
+    def printInfo(self):
+        print(f"Student = {self.name}, study program = {self.studyProgram}, age = {self.age}, gender = {self.gender}")
+    
+elena = Student("Elena", "class", 22, "female")
+alessia = Student("Elena", "class", 17, "female")
+erik = Student("Erik", "class", 30, "male")
+
+elena.printInfo()
