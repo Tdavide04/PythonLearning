88f1fbd3e854eac41476b4542f9336fdbbead32c
Davide <159887620+Tdavide04@users.noreply.github.com>
2024-05-11 19:38:33 +0200

    update

--- Project/ZooPark.py
+++ Project/ZooPark.py
@@ -1,1 +1,31 @@
-#hello
+class Zoo:
+    def __init__(self, zoo, zoo_keepers) -> None:
+        pass
+
+class Animal:
+    def __init__(self, name, species, age, height, width, preferred_habitat, health) -> None:
+        pass
+
+class Fence:
+    def __init__(self) -> None:
+        pass
+
+class ZooKeepers:
+    def __init__(self) -> None:
+        pass
+
+def add_animal(animal: Animal, fence: Fence):
+    pass
+
+def remove_animal(animal: Animal, fence: Fence):
+    pass
+
+def feed(animal: Animal):
+    pass
+
+def clean(fence: Fence):
+    pass
+
+def describe_zoo():
+    pass
+
