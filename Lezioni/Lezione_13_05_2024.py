0abd157a18a0a9f34eb062446a6329507edfe333
Nio4124 <antoniozottola41204@gmail.com>
2024-05-13 16:12:55 +0200

    ristudiarlo a casa

--- Lezioni/Lezione_13_05_2024.py
+++ Lezioni/Lezione_13_05_2024.py
@@ -10,3 +10,40 @@ class Zoo:
     def get_fences(self) -> list:
         return self.fences
     
+
+
+class Animal:
+
+    def __init__(self, name: str, age: int) -> None:
+        self.set_name(name)
+        self.set_age(age)
+
+    def set_name(self, name:str):
+        self.name = name
+
+    def set_age(self, age:int):
+        self.age = age
+
+    def __str__(self) -> str:
+        return f"Animal(name = {self.name}, age = {self.age}"
+    
+class Person(Animal):
+
+    def talk(self) -> str:
+        return f"Ciao, mi chiamo {self.name}"
+    
+    def __str__(self) -> str:
+        return f"Person(name = {self.name}, age = {self.age})"
+    
+class Student(Person):
+
+    def __init__(self, name: str, age: str, id: int):
+        super().__init__(name, age)
+        self.id = id
+
+    def talk(self) -> str:
+        s = super().talk()
+        return f"Ciao sono uno studente e mi chiamo {self.name}"
+    
+a = Student(name = "ufvbeug", age = 28, id = 1836)
+print(a.talk())
