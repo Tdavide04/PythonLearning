26168d3ebdfdb39af85cd938c6840f010f024ed6
Nio4124 <antoniozottola41204@gmail.com>
2024-05-15 17:22:23 +0200

    update

--- ITS/room.py
+++ ITS/room.py
@@ -1,27 +1,27 @@
 class Room:
-
-    def __init__(self, name: str, floor: int, num_seets: int) -> None:
-        self.set_name = name
+    
+    def __init__(self, name: str, floor: int, num_seats: int):
+        self.set_name(name)
         self.set_floor(floor)
-        self.set_num_seets(num_seets)
-
+        self.set_num_seats(num_seats)
+        
     def set_name(self, name: str):
-        self.name = name 
-
+        self.name = name
+    
     def set_floor(self, floor: int):
         self.floor = floor
-
-    def set_num_seets(self, num_seets: int):
-        self.num_seets = num_seets
-
+        
+    def set_num_seats(self, num_seats: int):
+        self.num_seats = num_seats
+        
     def get_name(self) -> str:
         return self.name
     
     def get_floor(self) -> int:
         return self.floor
     
-    def get_num_seets(self) -> int:
-        return self.num_seets
+    def get_num_seats(self) -> int:
+        return self.num_seats
     
-    def __str__(self):
-        pass
+    def __str__(self) -> str:
+        return f'Room(name={self.get_name()}, floor={self.get_floor()}, num_seats={self.get_num_seats()})'
