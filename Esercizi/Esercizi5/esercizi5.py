006fafa9aeadbbadbb554a901429f7db5056ad99
Tdavide04 <tdavide833@gmail.com>
2024-05-19 18:16:23 +0200

    update

--- Esercizi/Esercizi5/esercizi5.py
+++ Esercizi/Esercizi5/esercizi5.py
@@ -144,9 +144,57 @@ class Library():
 #Example: print(word_break("leetcode",["leet","code"]))
 # -> True
 
-def word_break(s: str, wordDict: list[str]) -> bool:
-    for e in wordDict:     
-        if e in s:
-             return True
-             wordDict.pop(e)
-    return False
+def word_break(s, wordDict):
+    dp = [False] * (len(s) + 1)
+    dp[0] = True
+
+    for i in range(1, len(s) + 1):
+        for j in range(i):
+            if dp[j] and s[j:i] in wordDict:
+                dp[i] = True
+                break
+
+    return dp[-1]
+        
+'''
+Progettare un semplice sistema bancario con i seguenti requisiti:
+
+Classe del Account:
+    Attributi:
+        account_id: str - identificatore univoco per l'account.
+        balance: float - il saldo attuale del conto.
+    Metodi:
+        deposit(amount: float) - aggiunge l'importo specificato al saldo del conto.
+        get_balance(): restituisce il saldo corrente del conto.
+Classe Bank:
+    Attributi:
+        accounts: dict[str, Account] - un dizionario per memorizzare gli account in base ai loro ID.
+    Metodi:
+        create_account(account_id): crea un nuovo account con l'ID specificato e un saldo pari a 0.
+        deposit(account_id, amount): deposita l'importo specificato sul conto con l'ID fornito.
+        get_balance(account_id): restituisce il saldo del conto con l'ID specificato.
+'''
+
+class Acoount:
+    def __init__(self, account_id: str, balance: float):
+        self.account_id = account_id
+        self.balance = balance
+        
+    def deposit(self, amount: float):
+        pass
+    
+    def get_balance(self):
+        pass
+    
+class Bank:
+    def __init__(self) -> None:
+        self.accounts = {}
+        
+    def create_account(self, account_id):
+        pass
+    
+    def deposit(self, account_id, amount):
+        pass
+    
+    def get_balance(self, account_id):
+        pass
