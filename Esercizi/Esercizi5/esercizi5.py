8e1959ee482808fee22fbef955b637bde2c3f377
Tdavide04 <tdavide833@gmail.com>
2024-05-19 21:34:53 +0200

    update

--- Esercizi/Esercizi5/esercizi5.py
+++ Esercizi/Esercizi5/esercizi5.py
@@ -1,217 +1,135 @@
-#Date due stringhe s e t, restituire True se t è un anagramma di s, e False altrimenti.
-#Un anagramma è una parola o una frase formata riorganizzando le lettere di una parola o frase diversa, 
-#in genere utilizzando tutte le lettere originali esattamente una volta.
-#Example: print(anagram("anagram","nagaram")
-# -> True
+#LEZIONE 5 ESERCIZI RIEPILOGATIVI E DI RIPASSO
 
-def anagram(s: str, t: str) -> bool:
-    s = s.lower()
-    t = t.lower()
-    if len(s) == len(t):
-        if sorted(s) == sorted(t):
+#Scrivi una funzione che riceve un numero e stampa un conto alla rovescia da quel numero a zero.
+#Example: countdown(5)
+# -> 5, 4, 3, 2, 1, 0
+
+def countdown(n: int) -> int:
+    lista: list = []
+    for numbers in range(n +1):
+        lista.append(numbers)
+    lista.reverse()
+    for element in lista:
+        print(element)
+    return lista
+
+#Scrivi una funzione che ruota gli elementi di una lista verso sinistra di un numero specificato k di posizioni. 
+#La rotazione verso sinistra significa che ciascun elemento della lista viene spostato a sinistra di una posizione e l'elemento iniziale viene spostato alla fine della lista. 
+#Per la rotazione utilizzare lo slicing e gestire il caso in cui il numero specificato di posizioni sia maggiore della lunghezza della lista.
+#Example: print(rotate_left([1, 2, 3, 4, 5], 2))
+# -> [3, 4, 5, 1, 2]
+
+def rotate_left(elements: list, k: int) -> list:
+    n = len(elements)
+    if k > n:
+        k = k%n
+    new_list = elements[k:] + elements[:k]
+    return new_list
+
+#Scrivi una funzione che verifica se una combinazione di condizioni (A, B, e C) è soddisfatta per procedere con un'operazione. 
+# L'operazione può procedere solo se la condizione A è vera o se entrambe le condizioni B e C sono vere. 
+# La funzione deve ritornare "Operazione permessa" oppure "Operazione negata" a seconda delle condizioni che sono soddisfatte.
+#Example: print(check_combination(True, False, True))
+# -> Operazione permessa
+
+def check_combination(conditionA: bool, conditionB: bool, conditionC: bool) -> str:
+    if conditionA:
+        return "Operazione permessa"
+    elif conditionB and conditionC:
+        return "Operazione permessa"
+    else:
+        return "Operazione negata"
+    
+#Scrivi una funzione che, data una lista, ritorni un dictionary che mappa ogni elemento alla sua frequenza nella lista.
+#Example: print(frequency_dict(['mela', 'banana', 'mela']))
+# -> {'mela': 2, 'banana': 1}
+
+def frequency_dict(elements: list) -> dict:
+    diz: dict = {}
+    for elem in elements:
+        if elem in diz:
+            diz[elem] += 1
+        else:
+            diz[elem] = 1
+    return diz
+
+#La funzione dovrebbe calcolare la media dei numeri in una lista di interi.
+#Un errore nell'implementazione porta a risultati inaspettati.
+#Example: print(calculate_average([1, 2, 3, 4, 5]))
+# -> 3.0
+
+def calculate_average(numbers: list[int]) -> float:
+    if len(numbers) == 0:
+        return 0
+    else:
+        return sum(numbers) / len(numbers)
+   
+#La funzione dovrebbe determinare se un elemento è presente in una lista.
+#Un errore nell'implementazione porta a risultati inaspettati.
+
+def find_element(lst: list[int], element: int) -> bool:
+    for item in lst:
+        if item == element:
             return True
-        return False
-    return False 
+    return False
 
-#Data l'inizio di una lista concatenata, invertire la lista e restituire la lista invertita.
-#Example: head = ListNode(val=1, next=ListNode(val=2, next=ListNode(val=3, next=ListNode(val=4, next=ListNode(val=5)))))
-#print(reverse_list(head))
-# -> [5, 4, 3, 2, 1]
-
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-        
-def reverse_list(head: ListNode) -> list[int]:
-    
-    val_list = []
-    current_node = head
-    while current_node != None:
-        val_list.append(current_node.val)
-        current_node = current_node.next
-    val_list.reverse()
-    return val_list
-
-'''
-Progettare un sistema di gestione della biblioteca con i seguenti requisiti:
-
-    Classe Book:
-        Attributi:
-            book_id: str - Identificatore di un libro.
-            title: str - titolo del libro.
-            author: str - autore del libro
-            is_borrowed: boolean - booleano che indica se il libro è in prestito o meno.
-        Metodi:
-            borrow()-Contrassegna il libro come preso in prestito se non è già preso in prestito.
-            return_book()- Contrassegna il libro come restituito.
-
-    Classe Member:
-        Attributi:
-            member_id: str - identificativo del membro.
-            name: str - il nome del membro.
-            borrowed_books: list[Book] - lista dei libri presi in prestito.
-        Metodi:
-            borrow_book(book): aggiunge il libro nella lista borrowed_books se non è già stato preso in prestito.
-            return_book(book): rimuove il libro dalla lista borrowed_books.
-
-    Classe Library:
-        Attributi:
-            books: dict[str, Book] - dizionario che ha per chiave l'id del libro e per valore l'oggetto Book
-            members: dict[str, Member] - dizionario che ha per chiave l'id del membro e per valore l'oggetto Membro
-        Methodi:
-            add_book(book_id: str, title: str, author: str): Aggiunge un nuovo libro nella biblioteca.
-            register_member(member_id:str, name: str): Iscrive un nuovo membro nella biblioteca.
-            borrow_book(member_id: str, book_id: str): Permette al membro di prendere in prestito il libro.
-            return_book(member_id: str, book_id: str): Permette al membro di restituire il libro.
-            get_borrowed_books(member_id): list[Book] - restituisce la lista dei libri presi in prestito dal membro.
-'''
-
-
-class Book:
-    def __init__(self, book_id: str, title: str, author: str):
-        self.book_id = book_id
-        self.title = title
-        self.author = author
-        self.is_borrowed = False
-
-    def borrow(self):
-        self.is_borrowed = True
-
-    def return_book(self):
-        self.is_borrowed = False
-
-class Member:
-    def __init__(self, member_id: str, name: str):
-        self.member_id = member_id
-        self.name = name
-        self.borrowed_books = []
-        
-    def borrow_book(self, book: Book):
-        if not book.is_borrowed:
-            self.borrowed_books.append(book)
-            book.borrow()
-        else:
-            raise ValueError("Book is already borrowed")
-
-    def return_book(self, book: Book):
-        if book not in self.borrowed_books:
-            raise ValueError("Book not borrowed by this member")            
-        self.borrowed_books.remove(book)
-        book.return_book()
-
-class Library():
-    def __init__(self):
-        self.books = {}
-        self.members = {}
-
-    def add_book(self, book_id: str, title: str, author: str): 
-        book = Book(book_id, title, author)
-        self.books[book_id] = book
-
-    def register_member(self, member_id:str, name: str): 
-        member = Member(member_id, name)
-        self.members[member_id] = member
-
-    def borrow_book(self, member_id: str, book_id: str):
-        if member_id in self.members and book_id in self.books:
-            member: Member = self.members[member_id]
-            book: Book = self.books[book_id]
-            member.borrow_book(book)
-        elif member_id not in self.members:
-            raise ValueError("Member not found")
-        else:
-            raise ValueError("Book not found")
-        
-    def return_book(self, member_id: str, book_id: str):
-        if member_id in self.members and book_id in self.books:
-            member: Member = self.members[member_id]
-            book: Book = self.books[book_id]
-            member.return_book(book)
-
-        
-
-    def get_borrowed_books(self, member_id) -> list[Book]:
-        if member_id in self.members:
-            member: Member = self.members[member_id]
-            list_title: list[str] = []
-            for book in member.borrowed_books:
-                list_title.append(book.title)
-            return list_title
-
-#Data una stringa s e una lista di stringhe wordDict, restituisce True se s può essere segmentato in una sequenza separata da spazi di una o più parole del dizionario; 
-# False altrimenti. Tieni presente che la stessa parola nel dizionario può essere riutilizzata più volte nella segmentazione.
-#Example: print(word_break("leetcode",["leet","code"]))
+#Scrivi una funzione che verifica se in una stringa le parentesi '(' e ')' sono bilanciate, 
+# cioè per ogni parentesi che apre c'è la corrispondente parentesi che chiude.
+#Example: print(check_parentheses("()()"))
 # -> True
 
-def word_break(s, wordDict):
-    dp = [False] * (len(s) + 1)
-    dp[0] = True
+def check_parentheses(expression: str) -> bool:
+    count = 0
+    for elem in expression:
+        if elem == "(":
+            count += 1
+        if elem == ")":
+            count -= 1
+        if count < 0:
+            return False
+    return count == 0
 
-    for i in range(1, len(s) + 1):
-        for j in range(i):
-            if dp[j] and s[j:i] in wordDict:
-                dp[i] = True
-                break
+#Scrivi una funzione che conta e ritorna quante volte un elemento appare isolato in una lista di numeri interi. 
+# Un elemento è considerato isolato se non è affiancato sia a destra che a sinistra da elementi uguali.
+#Example: print(count_isolated([1, 2, 2, 3, 3, 3, 4]))
+# -> 2
 
-    return dp[-1]
+def count_isolated(n) -> int:
+    count = 0
+    if n == []:
+        return count
+    else:
         
-'''
-Progettare un semplice sistema bancario con i seguenti requisiti:
-
-Classe del Account:
-    Attributi:
-        account_id: str - identificatore univoco per l'account.
-        balance: float - il saldo attuale del conto.
-    Metodi:
-        deposit(amount: float) - aggiunge l'importo specificato al saldo del conto.
-        get_balance(): restituisce il saldo corrente del conto.
-Classe Bank:
-    Attributi:
-        accounts: dict[str, Account] - un dizionario per memorizzare gli account in base ai loro ID.
-    Metodi:
-        create_account(account_id): crea un nuovo account con l'ID specificato e un saldo pari a 0.
-        deposit(account_id, amount): deposita l'importo specificato sul conto con l'ID fornito.
-        get_balance(account_id): restituisce il saldo del conto con l'ID specificato.
-'''
-
-class Account:
-    def __init__(self, account_id: str):
-        self.account_id = account_id
-        self.balance = 0.0
-        
-    def deposit(self, amount: float):
-        if amount > 0:
-            self.balance += amount  
-        else:
-            raise ValueError("L'importo del deposito deve essere positivo")
-        
-    def get_balance(self) -> float:
-        return self.balance
+        if n[0] != n[1]:
+            count += 1
+        for number in range(1, len(n) -1):
+            if n[number] != n[number + 1] and n[number] != n[number - 1]:
+                count += 1
+        if n[-1] != n[-2]:
+            count += 1
+        return count
     
-class Bank:
-    def __init__(self) -> float:
-        self.accounts = {}
-        
-    def create_account(self, account_id):
-        if account_id not in self.accounts:
-            if account_id not in self.accounts:
-                new_account = Account(account_id)
-                self.accounts[account_id] = new_account
-            return new_account 
+#Scrivi una funzione che, dato un insieme e una lista di numeri interi da rimuovere, ritorni un nuovo insieme senza i numeri specificati nella lista.
+#Example: print(remove_elements({5, 6, 7}, [7, 8, 9]))
+# -> {5, 6}
+
+def remove_elements(original_set: set[int], elements_to_remove: list[int]) -> set[int]:
+    for elem in elements_to_remove:
+        if elem in original_set:
+            original_set.remove(elem)
+    return original_set
+
+#Scrivi una funzione che unisce due dizionari. Se una chiave è presente in entrambi, somma i loro valori.
+#Example: print(merge_dictionaries({'x': 5}, {'x': -5}))
+# -> {'x': 0}
+
+def merge_dictionaries(dict1: dict, dict2: dict) -> dict:
+    merged_dict = {}
+    for key, value in dict1.items():
+        merged_dict[key] = value
+    for key, value in dict2.items():
+        if key in merged_dict:
+            merged_dict[key] += value
         else:
-            raise ValueError("L'account ID esiste già")
-    
-    def deposit(self, account_id, amount):
-        if account_id in self.accounts:
-            self.accounts[account_id].deposit(amount) 
-        else:
-            raise ValueError("Account ID non trovato")
-    
-    def get_balance(self, account_id):
-        if account_id in self.accounts:
-            return self.accounts[account_id].get_balance()
-        else:
-            raise ValueError("Account ID non trovato")
-        
-        
+            merged_dict[key] = value
+    return merged_dict
