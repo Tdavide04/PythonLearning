96b09d41c468def3ad406ea99be6087277e0577b
Tdavide04 <tdavide833@gmail.com>
2024-06-06 12:26:28 +0200

    update

--- Esercizi/Esercizi8/esercizi8.py
+++ Esercizi/Esercizi8/esercizi8.py
@@ -106,6 +106,8 @@ def merge(nums1, m, nums2, n):
     return nums1.sort()
     
 #Given the head of a singly linked list, return true if it is a palindrome. Model the Node and Linked List concepts using classes. 
+
+'''
 class LinkedList:
     def __init__(self):
         lista = []
@@ -122,10 +124,63 @@ ll1 = LinkedList()
 for value in [1, 2, 3, 2, 1]:
     ll1.append(value)
 print(is_palindrome(ll1.head))
+'''
 
 #Exercise 1: Creating an Abstract Class with Abstract Methods
 #Create an abstract class Shape with an abstract method area and another abstract method perimeter. 
 #Then, create two subclasses Circle and Rectangle that implement the area and perimeter methods.
 
 from abc import ABC, abstractmethod
+import math
 
+class Shape(ABC):
+    
+    @abstractmethod
+    def area(self):
+        pass
+    
+    @abstractmethod
+    def perimeter(self):
+        pass
+    
+class Circle(Shape):
+    
+    def __init__(self, radius: float):
+        self.radius = radius
+        
+    def area(self):
+        return math.pi * (self.radius ** 2)
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+class Rectangle(Shape):
+    
+    def __init__(self, length: float, width: float):
+       self.length = length
+       self.width = width
+       
+    def area(self):
+        return self.width * self.length
+    
+    def perimeter(self):
+        return 2 * (self.length + self.width)
+    
+#Exercise 2: Implementing Static Methods
+# Create a class MathOperations with a static method add that takes two numbers and returns their sum, 
+# and another static method multiply that takes two numbers and returns their product.
+
+class MathOperations:
+    
+    def __init__(self, a: float, b: float) -> None:
+        self.a = a
+        self.b = b
+    
+    @staticmethod
+    def add(a, b):       
+        return a + b
+    
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+    
