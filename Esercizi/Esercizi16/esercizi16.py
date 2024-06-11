136e768e66464f7573472d7b271d7653a1bffdb2
Tdavide04 <tdavide833@gmail.com>
2024-06-11 15:29:59 +0200

    Esercizio 3 svolto

--- Esercizi/Esercizi16/esercizi16.py
+++ Esercizi/Esercizi16/esercizi16.py
@@ -305,16 +305,140 @@ class RecipeManager:
         else:
             raise ValueError(f"'{ingredient}' does not exist in any recipe")
         
-'''        
-manager = RecipeManager()
-print(manager.create_recipe("Pizza Margherita", ["Farina", "Acqua", "Lievito", "Pomodoro", "Mozzarella"]))
-print(manager.add_ingredient("Pizza Margherita", "Basilico"))
-print(manager.update_ingredient("Pizza Margherita", "Mozzarella", "Mozzarella di Bufala"))
-print(manager.remove_ingredient("Pizza Margherita", "Acqua"))
-print(manager.list_ingredients("Pizza Margherita"))
-print(manager.create_recipe("Torta di mele", ["Farina", "Uova", "Mele"]))
-print(manager.add_ingredient("Torta di mele", "Zucchero"))
-print(manager.list_recipes()) # ['Torta di mele']
-print(manager.list_ingredients("Torta di mele"))
-print(manager.search_recipe_by_ingredient("Uova"))
+'''
+In questo esercizio, creeremo una gerarchia di classi per rappresentare diversi tipi di veicoli.
+1. Classe Base: Veicolo
+Crea una classe base chiamata Veicolo con i seguenti attributi e metodi:
+ 
+Attributi:
+
+    marca (stringa)
+    modello (stringa)
+    anno (intero)
+
+Metodi:
+
+    __init__(self, marca, modello, anno): metodo costruttore che inizializza gli attributi marca, modello e anno.
+    descrivi_veicolo(self): metodo che stampa una descrizione del veicolo nel formato "Marca: [marca], Modello: [modello], Anno: [anno]".
+
+2. Classe Derivata: Auto
+Crea una classe derivata chiamata Auto che eredita dalla classe Veicolo e aggiunge i seguenti attributi e metodi:
+ 
+Attributi:
+
+    numero_porte (intero)
+
+Metodi:
+
+    __init__(self, marca, modello, anno, numero_porte): metodo costruttore che inizializza gli attributi della classe base e numero_porte.
+    descrivi_veicolo(self): metodo che sovrascrive quello della classe base per includere anche il numero di porte nella descrizione, 
+    nel formato "Marca: [marca], Modello: [modello], Anno: [anno], Numero di porte: [numero_porte]".
+
+3. Classe Derivata: Moto
+Crea una classe derivata chiamata Moto che eredita dalla classe Veicolo e aggiunge i seguenti attributi e metodi:
+ 
+Attributi:
+
+    tipo (stringa, ad esempio "sportiva", "cruiser", ecc.)
+
+Metodi:
+
+    __init__(self, marca, modello, anno, tipo): metodo costruttore che inizializza gli attributi della classe base e tipo.
+    descrivi_veicolo(self): metodo che sovrascrive quello della classe base per includere anche il tipo di moto nella descrizione, 
+    nel formato "Marca: [marca], Modello: [modello], Anno: [anno], Tipo: [tipo]".
+
+Example: 
+veicolo = Veicolo("Generic", "Model", 2020)
+auto = Auto("Toyota", "Corolla", 2021, 4)
+moto = Moto("Yamaha", "R1", 2022, "sportiva")
+
+veicolo.descrivi_veicolo() 
+auto.descrivi_veicolo()
+moto.descrivi_veicolo()
+
+Result:
+Marca: Generic, Modello: Model, Anno: 2020
+Marca: Toyota, Modello: Corolla, Anno: 2021, Numero di porte: 4
+Marca: Yamaha, Modello: R1, Anno: 2022, Tipo: sportiva
+'''
+
+class Veicolo:
+
+    def __init__(self, marca: str, modello: str, anno: int): #metodo costruttore che inizializza gli attributi marca, modello e anno.
+        self.marca = marca
+        self.modello = modello
+        self.anno = anno
+
+    def descrivi_veicolo(self): #metodo che stampa una descrizione del veicolo nel formato "Marca: [marca], Modello: [modello], Anno: [anno]".
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}")
+    
+class Auto(Veicolo):
+
+    def __init__(self, marca: str, modello: str, anno: int, numero_porte: int): #metodo costruttore che inizializza gli attributi della classe base e numero_porte.
+        super().__init__(marca, modello, anno)
+        self.numero_porte =numero_porte
+
+    def descrivi_veicolo(self): 
+        '''
+        metodo che sovrascrive quello della classe base per includere anche il numero di porte nella descrizione, 
+        nel formato "Marca: [marca], Modello: [modello], Anno: [anno], Numero di porte: [numero_porte]".
+        '''
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}, Numero di porte: {self.numero_porte}")
+    
+class Moto(Veicolo):
+
+    def __init__(self, marca: str, modello: str, anno: int, tipo: str): #metodo costruttore che inizializza gli attributi della classe base e tipo.
+        super().__init__(marca, modello, anno)
+        self.tipo = tipo
+
+    def descrivi_veicolo(self): 
+        '''
+        metodo che sovrascrive quello della classe base per includere anche il tipo di moto nella descrizione, 
+        nel formato "Marca: [marca], Modello: [modello], Anno: [anno], Tipo: [tipo]".
+        '''
+        print(f"Marca: {self.marca}, Modello: {self.modello}, Anno: {self.anno}, Tipo: {self.tipo}")
+
+'''
+
+Obiettivo
+L'obiettivo di questo esercizio è creare un modello semplice per simulare la crescita delle popolazioni 
+di due specie animali usando la programmazione orientata agli oggetti in Python.
+
+Descrizione del problema
+Due specie animali, i Bufali Klingon e gli Elefanti, vivono in una riserva naturale. 
+Ogni specie ha una popolazione iniziale e un tasso di crescita annuo. Vogliamo sapere:
+- In quanti anni la popolazione degli Elefanti supererà quella dei Bufali Klingon.
+- n quanti anni la popolazione dei Bufali Klingon raggiungerà una densità di 1 individuo per km².
+ 
+Specifiche tecniche
+
+1. Classe Specie
+- Attributi:
+
+    nome (str): Nome della specie.
+    popolazione (int): Popolazione iniziale.
+    tasso_crescita (float): Tasso di crescita annuo percentuale.
+
+- Metodi:
+
+    __init__(self, nome: str, popolazione_iniziale: int, tasso_crescita: float): Costruttore per inizializzare gli attributi della classe.
+    cresci(self): Metodo per aggiornare la popolazione per l'anno successivo.
+    anni_per_superare(self, altra_specie: 'Specie') -> int: Metodo per calcolare in quanti anni la popolazione di questa specie supererà quella di un'altra specie.
+    getDensita(self, area_kmq: float) -> int: Metodo per calcolare in quanti anni la popolazione raggiungerà una densità di 1 individuo per km².
+
+ 
+
+2. Sottoclassi BufaloKlingon e Elefante
+Entrambe le sottoclassi animali BufaloKlingon ed Elefante devono ereditare dalla classe base Specie e devono inizializzare il nome della specie rispettiva.
+ 
+Formule Matematiche:
+
+    Aggiornamento della popolazione per l'anno successivo:
+        Formula: popolazione_nuova = popolazione_attuale x (1 + tasso_crescita/100)
+    Calcolo della densità di popolazione:
+        Formula: popolazione / area_kmq
+        Hint: Loop incrementale che continua ad aggiornare la popolazione finché la densità non raggiunge 1 individuo per km²
+    Calcolo degli anni necessari per superare la popolazione di un'altra specie:
+        Hint: Loop incrementale che continua ad aggiornare la popolazione di entrambe le specie finché la popolazione di questa specie non supera quella dell'altra. Per evitare che le popolazioni crescano all'infinito, limitare il numero di anni a 1000. 
+
 '''
