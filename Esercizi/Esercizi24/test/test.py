0a5ec146bbf2dc016b09864240f5c9bcf9aca785
Tdavide04 <tdavide833@gmail.com>
2024-07-10 18:31:07 +0200

    test

--- Esercizi/Esercizi24/test/test.py
+++ Esercizi/Esercizi24/test/test.py
@@ -11,5 +11,29 @@ class TestDocument(unittest.TestCase):
         doc.setText("Hello, world!")
         self.assertEqual(doc.getText(), "Hello, world!")
 
+    def test_get_text(self):
+        doc = Document()
+        doc.getText()
+
+    def test_isInText(self):
+        doc = Document()
+        doc.setText("Ciao 23")
+        doc.isInText("23")
+
+class TestEmail(unittest.TestCase):
+    def test_getText(self):
+        email = Email()
+        email.setText("Hello")
+        self.assertEqual(email.getText(), "Hello")
+
+    def test_writeToFile(self):
+        email = Email()
+        email.setText("Hellooo my friend")
+        email.writeToFile()
+        r = open("document.txt", "r")
+        f = r.read()
+        r.close()
+        self.assertEqual(f"Da: , A: \nTitolo: \nMessagio: {email.getText()}", f)
+
 if __name__ == "__main__":
     unittest.main()
