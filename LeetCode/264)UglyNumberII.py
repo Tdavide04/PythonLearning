7ec27743fac12c0594c1cffc35afa8ef0035f908
Davide <159887620+Tdavide04@users.noreply.github.com>
2024-08-01 01:15:36 +0200

    ìUglyNumberII

--- LeetCode/264)UglyNumberII.py
+++ LeetCode/264)UglyNumberII.py
@@ -27,33 +27,39 @@ Constraints:
 
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-
-        k = []
+        # Array per memorizzare i numeri brutti
+        ugly_numbers = [0] * n
+        ugly_numbers[0] = 1  # Il primo numero è sempre 1
         
-        for e in range(1, 5000):
-            count = 0
-            j = e
-            while e:   
-                if e == 1:
-                    k.append(e)
-                    break
-                if e % 2 == 0:
-                    e /= 2
-                elif e % 3 == 0:
-                    e /= 3
-                elif e % 5 == 0:
-                    e /= 5
-                if e == 1:
-                    k.append(j)
-                    break
-                count += 1
-                if count == 50:
-                    break
-        return k[n-1]
+        # Indici per i multipli di 2, 3, e 5
+        i2 = i3 = i5 = 0
+        
+        # Valori iniziali di moltiplicatori
+        next_multiple_of_2 = 2
+        next_multiple_of_3 = 3
+        next_multiple_of_5 = 5
+        
+        for i in range(1, n):
+            # Seleziona il prossimo numero brutto più piccolo, poichè essi vengono modificati durante l'esecuzione
+            next_ugly = min(next_multiple_of_2, next_multiple_of_3, next_multiple_of_5)
+            ugly_numbers[i] = next_ugly
             
+            # Incrementa gli indici rispettivi e aggiorna i moltiplicatori
+            if next_ugly == next_multiple_of_2:
+                i2 += 1
+                next_multiple_of_2 = ugly_numbers[i2] * 2
+            if next_ugly == next_multiple_of_3:
+                i3 += 1
+                next_multiple_of_3 = ugly_numbers[i3] * 3
+            if next_ugly == next_multiple_of_5:
+                i5 += 1
+                next_multiple_of_5 = ugly_numbers[i5] * 5
+        
+        return ugly_numbers[-1]
+
             
 
 if __name__ == "__main__":
 
     sos = Solution()
-    print(sos.nthUglyNumber(14))
+    print(sos.nthUglyNumber(144))
