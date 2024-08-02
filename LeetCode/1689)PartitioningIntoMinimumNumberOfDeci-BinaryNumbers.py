9ae5e041e495d735b1ff1e104e9cf4808ffd4f2c
Tdavide04 <tdavide833@gmail.com>
2024-08-02 19:53:30 +0200

    PartitioningIntoMinimumNumberOfDeci-BinaryNumbers

---
+++ LeetCode/1689)PartitioningIntoMinimumNumberOfDeci-BinaryNumbers.py
@@ -0,0 +1,7 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        return int(max(n))
+        
+if __name__ == "__main__":        
+    sos = Solution()
+    print(sos.minPartitions("27346209830709182346"))
