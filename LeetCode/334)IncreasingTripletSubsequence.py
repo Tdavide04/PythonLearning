ff15068d9088a9edc16d25a5505a58cdd9c86317
Tdavide04 <tdavide833@gmail.com>
2024-08-22 19:05:06 +0200

    Triplet Finished

--- LeetCode/334)IncreasingTripletSubsequence.py
+++ LeetCode/334)IncreasingTripletSubsequence.py
@@ -31,10 +31,30 @@ Follow up: Could you implement a solution that runs in O(n) time complexity and 
 
 class Solution:
     def increasingTriplet(self, nums: list[int]) -> bool:
-        pass
+        
+        # Initialize 'first' and 'second' with a large value, using the sum of all elements in nums plus the lenght of the list.
+        # This ensures that any element in nums will be smaller initially and can update 'first' or 'second'.
+        first = sum(nums) + len(nums)
+        second = sum(nums) + len(nums)
+        for i in nums:
+            if i < first: # If the current element is smaller than 'first', update 'first' to this element.
+                first = i
+            elif i > first and i < second: # If the current element is larger than 'first' but smaller than 'second', update 'second'.
+                second = i
+            elif i > second: # If the current element is larger than 'second', it means we've found a valid increasing triplet.
+                return True
+        return False
+                
+            
     
 if __name__ == "__main__":
     
     sos = Solution()
-    print(sos.increasingTriplet(nums = [2,1,5,0,4,6]))
-    print(sos.increasingTriplet(nums = [5,4,3,2,1]))
+    print(sos.increasingTriplet(nums = [6,7,1,2])) # False
+    print(sos.increasingTriplet(nums = [2,1,5,0,4,6])) # True
+    print(sos.increasingTriplet(nums = [5,4,3,2,1])) # False
+    print(sos.increasingTriplet(nums = [1,5,0,4,1,3])) # True
+    print(sos.increasingTriplet(nums = [20,100,10,12,5,13])) # True
+    print(sos.increasingTriplet(nums = [1])) # False
+    print(sos.increasingTriplet(nums = [0,4,2,1,0,-1,-3])) # False
+    print(sos.increasingTriplet(nums = [5,0,6])) # False
