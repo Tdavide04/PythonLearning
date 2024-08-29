fccdf3e03114b129ea0af0ba680fc1d40776d4af
Tdavide04 <tdavide833@gmail.com>
2024-08-29 09:52:01 +0200

    Contains Duplicate II

--- LeetCode/219)ContainsDuplicateII.py
+++ LeetCode/219)ContainsDuplicateII.py
@@ -26,14 +26,17 @@ Constraints:
 
 class Solution:
     def containsNearbyDuplicate(self, nums: list[int], k: int) -> bool:
-        left: int = 0
-        for right in range(len(nums)):
-            if nums[left] == nums[right] and left != right:
-                if abs(left-right) <= k:
+        diz: dict[int:int] = {} # Create a dictionary to store the last seen index of each number
+        for i, num in enumerate(nums): # Iterate over the list with both index and value
+            if num not in diz:
+                diz[num] = i
+            else:
+                if abs(i - diz[num]) <= k: # If the number is already in the dictionary, check the distance
                     return True
                 else:
-                    left += 1
+                    diz[num] = i
         return False
+                
     
     
 if __name__ == "__main__":
@@ -41,3 +44,15 @@ if __name__ == "__main__":
     sos = Solution()
     print(sos.containsNearbyDuplicate(nums = [1,2,3,1], k = 3))
     print(sos.containsNearbyDuplicate(nums = [1,0,1,1], k = 1))
+    
+'''
+Strategy
+The strategy for solving the problem involves using a dictionary to track the last index of each element encountered in the list. 
+As you iterate through the list:
+Check Existence: For each element, check if it has been seen before by looking it up in the dictionary.
+Distance Check: If the element has been seen before, calculate the difference between the current index and the stored index.
+If the difference is less than or equal to the given threshold k, return True as the condition is met.
+Update Index: If the difference exceeds k or the element was not previously seen, update the dictionary with the current index of the element.
+Completion: If the iteration completes without finding any qualifying duplicates, return False.
+This approach ensures efficient checking and updating of elements while maintaining a time complexity of O(n), where n is the number of elements in the list.
+'''
