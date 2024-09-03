19b21b584dc0d80e45f0ed2f39f4015b03e81a33
Tdavide04 <tdavide833@gmail.com>
2024-09-03 17:33:37 +0200

    Find the Power of K-Size Subarrays I Finished

--- Lezioni/Lezione_3_09_2024/CercaStringa/cerca.py
+++ Lezioni/Lezione_3_09_2024/CercaStringa/cerca.py
@@ -1,15 +1,24 @@
 import os
 
-def CercaParolaInNomeFile(sFile, sParola):
-    sFile.find("pippo")
+def CercaParolaInNomeFile(sFile, sParola) -> bool:
+    sFileLower = sFile.lower()
+    sParolaLower = sParola.lower()
+    if sFile.find(sParolaLower) >= 0:
+        return True
+    return False
 
-sRoot = input("inserisci la directory dove cercare: ")
-sParola = input("inserisci la parola da cercare: ")
-sOutDir = input("inserisci la directory dove mettere i file trovati: ")
+sRoot = input("Inserisci la directory dove cercare: ")
+sParola = input("Inserisci la parola da cercare: ")
+sOutDir = input("Inserisci la directory dove mettere i file trovati: ")
 
 bRet = False
 for root, ListDir, ListFiles in os.walk(sRoot):
     print(f"Nella directory {root} ci sono {len(ListDir)} sottodirectory e {len(ListFiles)} files")
     for file in ListFiles:
         print(f"Devo cercare {sParola} in {file}")
-        bRet = CercaParolaInNomeFile(file, sParola)
+        bRet = CercaParolaInNomeFile(file, sParola) 
+        if bRet:
+            print(f"Trovata parola in file {file}")
+        else:
+            sFilePathCompleto = os.path.join(root, file)
+            bRet = CercaParolaInContenutoFile(sFilePathCompleto, sParola)
