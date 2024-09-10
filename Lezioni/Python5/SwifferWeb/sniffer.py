81487b85b337d3312883b2e08d3a71ec8b2632a0
Tdavide04 <tdavide833@gmail.com>
2024-09-10 13:42:22 +0200

    update

--- Lezioni/Python5/SwifferWeb/sniffer.py
+++ Lezioni/Python5/SwifferWeb/sniffer.py
@@ -1,10 +1,11 @@
-from scapy.all import *
+from scapy.all import sniff
 from scapy.layers.inet import IP
 from scapy.layers.http import HTTPRequest, HTTPResponse
 
 iPkt: int = 0
 def process_pkt(pkt):
+    global iPkt
     iPkt += 1
     print(f"Ho ricevuto un pacchetto {iPkt} lungo {pkt[IP].len}")
 
-sniff(ifaces="eth0", filter="tcp", prn= process_pkt)
+sniff(iface="eth0", filter="tcp", prn= process_pkt)
