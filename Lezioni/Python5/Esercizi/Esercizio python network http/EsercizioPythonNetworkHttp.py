b78c7867a5861583533ccf6f0d2b440f92090ce9
Tdavide04 <tdavide833@gmail.com>
2024-09-13 10:23:00 +0200

    update

--- Lezioni/Python5/Esercizi/Esercizio python network http/EsercizioPythonNetworkHttp.py
+++ Lezioni/Python5/Esercizi/Esercizio python network http/EsercizioPythonNetworkHttp.py
@@ -1,7 +1,10 @@
-from scapy.all import sniff
-from scapy.layers.inet import IP
+from scapy.all import *
+from scapy.layers.inet import IP, TCP
 from scapy.layers.http import HTTPRequest, HTTPResponse
 from datetime import datetime
+
+
+
 iPkt: int = 0
 def process_pkt(pkt):
     global iPkt
@@ -9,9 +12,12 @@ def process_pkt(pkt):
     data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     ip_src = pkt[IP].src
     ip_dst = pkt[IP].dst
-    tcp_src = pkt[IP].sport
-    tcp_dst = pkt[IP].dport
-    host = 0
+    tcp_src = pkt[TCP].sport
+    tcp_dst = pkt[TCP].dport
+    if pkt.haslayer(HTTPRequest):
+        host = pkt[HTTPRequest].Host
+    else:
+        host = "Unknown"
     
     with open("packet.csv", "a") as file:
         file.write(f"Numero: {iPkt}, Data: {data}, Ip_src: {ip_src}, Ip_dst: {ip_dst}, Tcp_src: {tcp_src}, Tcp_dst: {tcp_dst}, Host: {host}\n")
