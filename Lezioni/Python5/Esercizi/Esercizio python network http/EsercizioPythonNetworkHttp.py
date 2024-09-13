98f57544413397d5fed7fda4e4b107e4014a5f69
Tdavide04 <tdavide833@gmail.com>
2024-09-13 10:53:34 +0200

    update

--- Lezioni/Python5/Esercizi/Esercizio python network http/EsercizioPythonNetworkHttp.py
+++ Lezioni/Python5/Esercizi/Esercizio python network http/EsercizioPythonNetworkHttp.py
@@ -7,22 +7,22 @@ import csv
 
 
 iPkt: int = 0
-def process_pkt(pkt):
-    global iPkt
-    iPkt += 1
-    data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    ip_src = pkt[IP].src
-    ip_dst = pkt[IP].dst
-    tcp_src = pkt[TCP].sport
-    tcp_dst = pkt[TCP].dport
-    if pkt.haslayer(HTTPRequest):
-        host = pkt[HTTPRequest].Host
-    else:
-        host = "Unknown"
-    with open("packet.csv", "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(["Data", "IP Sorgente", "IP Destinatario", "TCP Sorgente", "TCP Destinatario", "Host"])
-        writer.writerow([data, ip_src, ip_dst, tcp_src, tcp_dst, host])
-    print(f"Pacchetto {iPkt}. Specifiche: data-ora: {data}, ip_src: {ip_src}, ip_dst: {ip_dst}, tcp_src: {tcp_src}, tcp_dst: {tcp_dst}, host: {host}")
+with open("packet.csv", "a") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Data", "IP Sorgente", "IP Destinatario", "TCP Sorgente", "TCP Destinatario", "Host"])        
+    def process_pkt(pkt):
+        global iPkt
+        iPkt += 1
+        data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        ip_src = pkt[IP].src
+        ip_dst = pkt[IP].dst
+        tcp_src = pkt[TCP].sport
+        tcp_dst = pkt[TCP].dport
+        if pkt.haslayer(HTTPRequest):
+            host = pkt[HTTPRequest].Host
+        else:
+            host = "Unknown"
+            writer.writerow([data, ip_src, ip_dst, tcp_src, tcp_dst, host])
+        print(f"Pacchetto {iPkt}. Specifiche: data-ora: {data}, ip_src: {ip_src}, ip_dst: {ip_dst}, tcp_src: {tcp_src}, tcp_dst: {tcp_dst}, host: {host}")
 
-sniff(iface="eth0", filter="tcp", prn= process_pkt)
+    sniff(iface="eth0", filter="tcp", prn= process_pkt)
