b7813ef7fbb36bf3116b97e04ce4288e7953570f
Tdavide04 <tdavide833@gmail.com>
2024-09-27 11:27:53 +0200

    update

--- Lezioni/Python5/Rest/server.py
+++ Lezioni/Python5/Rest/server.py
@@ -1,6 +1,8 @@
 from flask import Flask, json, request
 
 api = Flask(__name__)
+file_path = "Lezioni/Python5/Rest/file.json"
+
 
 @api.route("/add_cittadino", methods=["POST"])
 def gestisciAddCittadino():
@@ -14,6 +16,68 @@ def gestisciAddCittadino():
         return json.dumps(jsonResp)
     else:
         return "Content-Type not supported!"
+    
+def addDatiCittadino(nome, cognome, dataN, codF):
+    new_cittadino = {codF:{
+        "nome": nome,
+
+        "cognome": cognome,
+        "dataNascita": dataN,
+        "codFiscale": codF,
+    }}
+    try: 
+        with open(file_path) as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        return "Error, no data found"
+    
+    if codF not in data:
+        data.update(new_cittadino)
+        with open(file_path, "w") as file:
+            json.dump(data, file)
+    else:
+        return "Error, cittadino already registered"
+
+def readDatiCittadino(codF) -> dict:
+    try: 
+        with open(file_path) as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        return "Error, no data found"
+    if codF in data:
+        return data[codF]
+    
+def updateDatiCittadino(codF, oldValue, newValue):
+    try: 
+        with open(file_path) as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        return "Error, no data found"
+    
+    if codF in data:
+        if oldValue in data:
+            data[codF][oldValue] = newValue
+            with open(file_path, "w") as file:
+                json.dump(data, file)
+            return "Data updated"
+        else:
+            return "Data not found"
+    else:
+        return "Error, cittadino is not registered"
+    
+def deleteDatiCittadino(codF):
+    try:
+        with open(file_path) as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        return "Error, no data found"
+    if codF in data:
+        del data[codF]
+        with open(file_path, "w") as file:
+            json.dump(data)
+    else:
+        return "Error, cittadino is not registered"
+
 
 if __name__ == "__main__":
 
