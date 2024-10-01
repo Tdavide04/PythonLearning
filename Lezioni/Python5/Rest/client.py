6898f10d318014ab0a76dea6b3e4aa28e4796469
Tdavide04 <tdavide833@gmail.com>
2024-10-01 09:58:14 +0200

    update

--- Lezioni/Python5/Rest/client.py
+++ Lezioni/Python5/Rest/client.py
@@ -1,34 +1,75 @@
-import requests,json
+import requests, json
+import sys
 
 base_url = "http://127.0.0.1:8080"
 
-
-def GetDatiCittadino():
-    nome = "Mario"
-    cognome = "Garibaldi"
-    dataN = "07/11/1890"
-    codF = "dcfvfg70b34h501u"
-    datiCittadino = {"nome":nome, "cognome": cognome, "dataNascita":dataN, "codFiscale":codF}
+def CreateDatiCittadino():
+    nome = input("Qual'è il nome?")
+    cognome = input("Qual'è il cognome")
+    dataN = input("Qual'è la data di nascita?")
+    codF = input("Qual'è il codice fiscale?")
+    """
+    {
+        "nome": "Mario",
+        "cognome":"Retti",
+        "data nascita": "22/05/2010",
+        "codice fiscale": "dfrcde23t44h501u"
+    }
+    """
+    datiCittadino = {"nome":nome, "cognome": cognome, "data nascita":dataN, "codice fiscale":codF}
     return datiCittadino
 
-def GetCodiceFiscaleCittadino():
-    codFiscale = input("Quale codice fiscale vuoi? ")
-    datiCittadino = {"codFiscale":codFiscale}
-    return datiCittadino
+def ReadDatiCittadino():
+    codF = input("Qual'è il codice fiscale?")
 
 
 
-cOper = input("Cosa vuoi fare? ")
-if cOper =="C":
-    api_url = base_url + "/add_cittadino"
-    jsonDataRequest = GetDatiCittadino()
-if cOper == "R":
-    api_url = base_url + "/read_cittadino"
-    jsonDataRequest = GetCodiceFiscaleCittadino()
 
-response = requests.post(api_url,json=jsonDataRequest)
-print(response.json())
-print(response.status_code)
-print(response.headers["Content-Type"])
-data1 = response.json()
-print(data1)
+print("Operazioni disponibili:")
+print("1. Inserisci cittadino (es. atto di nascita)")
+print("2. Richiedi cittadino (es. cert. residenza)")
+print("3. Modifica cittadino (es. cambio residenza)")
+print("4. Elimina cittadino (es. trasferim altro comune)")
+print("5. Esci")
+sOper = input("Cosa vuoi fare?")
+while(True):
+    if sOper == "1":
+        print("Richiesto atto di nascita")
+        api_url = base_url + "/add_cittadino"
+        jsonDataRequest = CreateDatiCittadino()
+        try:
+            response = requests.post(api_url,json=jsonDataRequest)
+        
+            #print(response.json())
+            print(response.status_code)
+            print(response.headers["Content-Type"])
+            data1 = response.json()
+            print(data1)
+        except:
+            print("Problemi di comunicazione con il server, riprovapiù tardi")
+
+    if sOper == "2":
+        print("Richiesto codice fiscale")
+        api_url = base_url + "/read_cittadino"
+        jsonDataRequest = ReadDatiCittadino()
+        try:
+            response = requests.get(api_url,json=jsonDataRequest)
+        
+            #print(response.json())
+            print(response.status_code)
+            print(response.headers["Content-Type"])
+            data1 = response.json()
+            print(data1)
+        except:
+            print("Problemi di comunicazione con il server, riprovapiù tardi")
+
+    if sOper=="5":
+        print("Buona giornata!")
+        sys.exit()
+    print("Operazioni disponibili:")
+    print("1. Inserisci cittadino (es. atto di nascita)")
+    print("2. Richiedi cittadino (es. cert. residenza)")
+    print("3. Modifica cittadino (es. cambio residenza)")
+    print("4. Elimina cittadino (es. trasferim altro comune)")
+    print("5. Esci")
+    sOper = input("Cosa vuoi fare?")    
--- Lezioni/Python5/Rest/server.py
+++ Lezioni/Python5/Rest/server.py
@@ -1,85 +1,37 @@
 from flask import Flask, json, request
+from myjson import JsonSerialize,JsonDeserialize
 
+sAnagrafe = "./anagrafe.json"
 api = Flask(__name__)
-file_path = "Lezioni/Python5/Rest/file.json"
+
+#mettere una lista di liste dove ogni lista è un cittadino
+
+#la chiave è il codice fiscale
+#add cittadino
+#read cittadino
+#update cittadino
+#delete cittadino
 
 
-@api.route("/cittadino", methods=["POST"])
-def gestisciAddCittadino():
-    content_type = request.headers.get("Content-Type")
-    print("Ricevuta chimata" + content_type)
-    if content_type == "application/json":
+@api.route('/add_cittadino', methods=['POST'])
+def GestisciAddCittadino():
+    content_type = request.headers.get('Content-Type')
+    print("Ricevuta chiamata " + content_type)
+    if (content_type == 'application/json'):
         jsonReq = request.json
-        print(jsonReq)
-        # cittadini.append(jsonReq)
-        jsonResp = {"Esito":"200", "Msg":"ok"}
-        return json.dumps(jsonResp)
-    else:
-        return "Content-Type not supported!"
-    
-@api.route("/add_cittadino", methods=["POST"])
-def addDatiCittadino(nome, cognome, dataN, codF):
-    new_cittadino = {codF:{
-        "nome": nome,
-        "cognome": cognome,
-        "dataNascita": dataN,
-        "codFiscale": codF,
-    }}
-    try: 
-        with open(file_path) as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        return "Error, no data found"
-    
-    if codF not in data:
-        data.update(new_cittadino)
-        with open(file_path, "w") as file:
-            json.dump(data, file)
-    else:
-        return "Error, cittadino already registered"
-
-@api.route("/read_cittadino", methods=["POST"])
-def readDatiCittadino(codF) -> dict:
-    try: 
-        with open(file_path) as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        return "Error, no data found"
-    if codF in data:
-        return data[codF]
-    
-def updateDatiCittadino(codF, oldValue, newValue):
-    try: 
-        with open(file_path) as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        return "Error, no data found"
-    
-    if codF in data:
-        if oldValue in data:
-            data[codF][oldValue] = newValue
-            with open(file_path, "w") as file:
-                json.dump(data, file)
-            return "Data updated"
+        sCodiceFiscale = jsonReq["codice fiscale"]
+        anagrafe = JsonDeserialize(sAnagrafe)
+        if sCodiceFiscale not in anagrafe:
+            dNuovoCittadino = jsonReq
+            anagrafe[sCodiceFiscale] = dNuovoCittadino
+            JsonSerialize(anagrafe,sAnagrafe)
+            jsonResp = {"Esito":"000", "Msg":"ok"}
+            return json.dumps(jsonResp),200
         else:
-            return "Data not found"
+            jsonResp = {"Esito":"001", "Msg":"Cittadino gia presente"}
+            return json.dumps(jsonResp),200
     else:
-        return "Error, cittadino is not registered"
-    
-def deleteDatiCittadino(codF):
-    try:
-        with open(file_path) as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        return "Error, no data found"
-    if codF in data:
-        del data[codF]
-        with open(file_path, "w") as file:
-            json.dump(data)
-    else:
-        return "Error, cittadino is not registered"
+        return 'Content-Type not supported!',401
 
 
-if __name__ == "__main__":
-
-    api.run(host="127.0.0.1", port=8080)
+api.run(host="127.0.0.1", port=8080)
