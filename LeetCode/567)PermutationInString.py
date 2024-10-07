0c164b346c0d674ba92becadcac836c408d5dcbe
Tdavide04 <tdavide833@gmail.com>
2024-10-07 17:15:30 +0200

    Permutation in String Finished

--- LeetCode/567)PermutationInString.py
+++ LeetCode/567)PermutationInString.py
@@ -1,3 +1,31 @@
+'''
+Given two strings s1 and s2, return true if s2 contains a
+permutation
+of s1, or false otherwise.
+
+In other words, return true if one of s1's permutations is the substring of s2.
+
+ 
+
+Example 1:
+
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+
+Example 2:
+
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+
+ 
+
+Constraints:
+
+    1 <= s1.length, s2.length <= 104
+    s1 and s2 consist of lowercase English letters.
+'''
+
 from collections import Counter
 
 class Solution:
@@ -8,13 +36,16 @@ class Solution:
         window = s2[:k]
         if Counter(s1) == Counter(window):
             return True
-        for i in range(len(s2[k:])):
+        s2 = s2[k:]
+        for i in range(len(s2)):
             window.pop(0)
             window.append(s2[i])
             if Counter(s1) == Counter(window):
                 return True
-        return False
+        return False 
     
 
-sos = Solution()
-print(sos.checkInclusion(s1="ab",s2="eidbaooo"))
+if __name__ == "__main__":
+    
+    sos = Solution()
+    print(sos.checkInclusion(s1="abc",s2="bbbca"))
