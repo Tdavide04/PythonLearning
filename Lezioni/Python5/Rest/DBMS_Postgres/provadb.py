66158ffd0a0aee26944ee8ae694d0dda2dc3540e
Tdavide04 <tdavide833@gmail.com>
2024-10-10 19:07:41 +0200

    update

--- Lezioni/Python5/Rest/DBMS_Postgres/provadb.py
+++ Lezioni/Python5/Rest/DBMS_Postgres/provadb.py
@@ -13,10 +13,10 @@ if cur is None:
 	print("Errore connessione al DB")
 	sys.exit()
 
-sQuery = "insert into cittadini ..........."
+sQuery = "insert into Cittadini (nome, cognome, dataNascita, codFiscale) values ('Walter', 'Albano', '1890-12-07', 'dcfvff70b34h501u')"
 db.write_in_db(cur, sQuery)
 
-sQuery = "select * from Cittadini limit 5;"
+sQuery = "select * from Cittadini;"
 iNumRows = db.read_in_db(cur,sQuery)
 for ii in range(0,iNumRows):
 	myrow = db.read_next_row(cur)
