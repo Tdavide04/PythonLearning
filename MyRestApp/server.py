43b604fd7e3c9b94b481708643bbb857179e5c93
Tdavide04 <tdavide833@gmail.com>
2024-11-11 13:19:30 +0100

    update

--- MyRestApp/server.py
+++ MyRestApp/server.py
@@ -1,24 +1,25 @@
 from flask import Flask, jsonify, request
 from configparser import ConfigParser
-from dbclient import db
+import dbclient as db
 import psycopg2
 import os, sys
 
 api = Flask(__name__)
 
-@api.route("/login", method = ["POST"])
+@api.route("/login", methods=["POST"])
+
 def login():
     connection = db.connect()
     if connection is None:
         print("Connessione al DB fallita")
         sys.exit()
+        
     try:
-        content_type = request.headers.get("Content-Type")
         jsonReq = request.json
         id = jsonReq.get("operator_id")
         password = jsonReq.get("operator_password")
-        login_query = (f"selcet id.password from operator where id = {id} and password = {password}")
-        if db.read_next_row(connection)[1]:
+        login_query = (f"select id.password from operator where id = {id} and password = {password}")
+        if db.read_in_db(connection, login_query) != -1:
             access = True
             operator = {"id" : id, "password" : password}
             print(f"Benvenuto operatore {id}")
