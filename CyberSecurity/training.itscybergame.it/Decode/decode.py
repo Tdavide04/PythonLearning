38aded071ca347dacb8df6d3f956f4328d068e45
Tdavide04 <tdavide833@gmail.com>
2025-03-10 17:07:16 +0100

    update

--- CyberSecurity/training.itscybergame.it/Decode/decode.py
+++
@@ -1,8 +0,0 @@
-import zlib
-
-with open('_decode.png.extracted/4FA.zlib', 'rb') as file:
-    compressed_data = file.read()
-    decompressed_data = zlib.decompress(compressed_data)
-
-with open('decompressed_data.txt', 'wb') as output:
-    output.write(decompressed_data)
